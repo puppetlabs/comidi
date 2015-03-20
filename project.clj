52e879a3f19107858a63dac3a9d21bcccb2b4487
Chris Price <chris@puppetlabs.com>
2015-03-20 08:34:21 -0700

    (MAINT) Update to released version of bidi

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Puppet Labs utility functions for use with the bidi web routing library"
   :url "https://github.com/puppetlabs/pl-bidi"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [bidi "1.18.8-SNAPSHOT"]
+                 [bidi "1.18.9"]
                  [compojure "1.3.2"]
                  [prismatic/schema "0.2.2"]
                  [puppetlabs/kitchensink "1.1.0"]])
