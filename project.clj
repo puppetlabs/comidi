6d596eef6028293e2d276ddca3e44f6052c20992
Chris Price <chris@puppetlabs.com>
2015-04-14 14:37:35 +0100

    (MAINT) specify explicit versions for common deps
    compojure and bidi share a few common deps.  This commit pins
    explicit versions of those deps so that downstream projects
    won't always be forced to deal with the conflicts.

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "Puppet Labs utility functions and compojure-like wrappers for use with the bidi web routing library"
   :url "https://github.com/puppetlabs/comidi"
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.reader "0.8.9"]
+                 [ring/ring-core "1.3.2"]
                  [bidi "1.18.9"]
                  [compojure "1.3.2"]
                  [prismatic/schema "0.4.0"]
