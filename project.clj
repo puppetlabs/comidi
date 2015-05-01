6df186c0b76fb5d9cab9772b654c0f3906b14270
Chris Price <chris@puppetlabs.com>
2015-05-01 12:52:34 +0100

    (MAINT) add nexus deploy info

--- project.clj
+++ project.clj
@@ -1,10 +1,23 @@
+(defn deploy-info
+  [url]
+  { :url url
+    :username :env/nexus_jenkins_username
+    :password :env/nexus_jenkins_password
+    :sign-releases false })
+
 (defproject puppetlabs/comidi "0.1.0-SNAPSHOT"
   :description "Puppet Labs utility functions and compojure-like wrappers for use with the bidi web routing library"
   :url "https://github.com/puppetlabs/comidi"
+
+  :pedantic? :abort
+
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.reader "0.8.9"]
                  [ring/ring-core "1.3.2"]
                  [bidi "1.18.9"]
                  [compojure "1.3.2"]
                  [prismatic/schema "0.4.0"]
-                 [puppetlabs/kitchensink "1.1.0"]])
+                 [puppetlabs/kitchensink "1.1.0"]]
+
+  :deploy-repositories [["releases" ~(deploy-info "http://nexus.delivery.puppetlabs.net/content/repositories/releases/")]
+                        ["snapshots" ~(deploy-info "http://nexus.delivery.puppetlabs.net/content/repositories/snapshots/")]])
