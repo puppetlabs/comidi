65c7956f59f8747391daf5d8aff2ab7800c4a5f2
Jenkins CI <ci@puppetlabs.com>
2015-08-25 09:15:05 -0700

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/comidi "0.2.0-SNAPSHOT"
+(defproject puppetlabs/comidi "0.2.0"
   :description "Puppet Labs utility functions and compojure-like wrappers for use with the bidi web routing library"
   :url "https://github.com/puppetlabs/comidi"
 
