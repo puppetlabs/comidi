645bd50ddad0bcb2c43ccf446cae5f391fa9a0e6
Chris Price <chris@puppetlabs.com>
2015-09-13 11:11:35 -0700

    (MAINT) Update version to 0.2.2-SNAPSHOT for dev

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/comidi "0.2.1"
+(defproject puppetlabs/comidi "0.2.2-SNAPSHOT"
   :description "Puppet Labs utility functions and compojure-like wrappers for use with the bidi web routing library"
   :url "https://github.com/puppetlabs/comidi"
 
