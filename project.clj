fabc6d83331fbf5af56e6f76f917ef4586dbf294
Dan Lidral-Porter <dlp@puppet.com>
2017-02-24 10:47:41 -0800

    (maint) Update deps.
    Update all of our dependencies as far as we can before running into
    transitive dependency conflicts (compojure > 1.5.0 introduces transitive
    conflicts between bidi and compojure in the version of ring-core).

--- project.clj
+++ project.clj
@@ -4,17 +4,13 @@
 
   :pedantic? :abort
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 
-                 ;; begin version conflict resolution dependencies
-                 [clj-time "0.10.0"]
-                 ;; end version conflict resolution dependencies
+  :dependencies [[org.clojure/clojure "1.8.0"]
 
-                 [bidi "1.23.1" :exclusions [org.clojure/clojurescript]]
-                 [compojure "1.4.0"]
-                 [prismatic/schema "1.0.4"]
+                 [bidi "2.0.12" :exclusions [org.clojure/clojurescript]]
+                 [compojure "1.5.0"]
+                 [prismatic/schema "1.1.3"]
 
-                 [puppetlabs/kitchensink "1.1.0"]]
+                 [puppetlabs/kitchensink "2.1.0"]]
 
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :username :env/clojars_jenkins_username
