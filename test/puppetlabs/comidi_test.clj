bbe69187422eb251cacee6cf0f65bd4339cc2427
Patrick Carlisle <patrick.carlisle@puppet.com>
2019-06-26 15:46:11 -0700

    (maint) Adjust indentation for require fix

--- test/puppetlabs/comidi_test.clj
+++ test/puppetlabs/comidi_test.clj
@@ -1,10 +1,10 @@
 (ns puppetlabs.comidi-test
   (:require [clojure.test :refer :all]
-           [puppetlabs.comidi :as comidi :refer :all]
-           [schema.test :as schema-test]
-           [schema.core :as schema]
-           [clojure.zip :as zip]
-           [bidi.bidi :as bidi]))
+            [puppetlabs.comidi :as comidi :refer :all]
+            [schema.test :as schema-test]
+            [schema.core :as schema]
+            [clojure.zip :as zip]
+            [bidi.bidi :as bidi]))
 
 (use-fixtures :once schema-test/validate-schemas)
 
